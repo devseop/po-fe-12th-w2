3610265c545a6e37643c201fbe283d6ffb870289
devseop <cocc263@gmail.com>
2023-08-30 20:20:17 +0900

    DESIGN: CommonHeader.tsx UI 스타일링

--- src/components/CommonHeader.tsx
+++ src/components/CommonHeader.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { API_URL } from '../constant/urls';
 import { styled } from 'styled-components';
+import { GoRepo } from 'react-icons/go';
 
 const CommonHeader = () => {
   return (
     <Header>
+      <GoRepo color='white' />
       <h1>{`${API_URL.owner} / ${API_URL.repo}`}</h1>
     </Header>
   );
@@ -12,9 +14,11 @@ const CommonHeader = () => {
 
 const Header = styled.header`
   text-align: center;
-  background-color: black;
   padding: 20px;
-  margin-bottom: 24px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  justify-content: center;
 
   h1 {
     font-size: 20px;

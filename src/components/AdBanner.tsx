2c2b5e8178d3ee6d847551609308b14700972d82
devseop <cocc263@gmail.com>
2023-08-30 17:40:38 +0900

    DESIGN: UI 스타일링

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AD_URL } from '../constant/urls';
+import { styled } from 'styled-components';
 
 const AdBanner = () => {
   return (
-    <Link to={AD_URL.to}>
-      <img src={AD_URL.img} alt='광고' />
-    </Link>
+    <Banner>
+      <Link to={AD_URL.to}>
+        <img src={AD_URL.img} alt='광고' />
+      </Link>
+    </Banner>
   );
 };
+
+const Banner = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+`;
 
 export default AdBanner;
--- src/components/CommonHeader.tsx
+++ src/components/CommonHeader.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { API_URL } from '../constant/urls';
+import { styled } from 'styled-components';
 
 const CommonHeader = () => {
-  return <header>{`${API_URL.owner}/${API_URL.repo}`}</header>;
+  return (
+    <Header>
+      <h1>{`${API_URL.owner} / ${API_URL.repo}`}</h1>
+    </Header>
+  );
 };
+
+const Header = styled.header`
+  text-align: center;
+  background-color: black;
+  padding: 20px;
+  margin-bottom: 24px;
+
+  h1 {
+    font-size: 20px;
+    color: #f0f6fc;
+  }
+`;
 
 export default CommonHeader;

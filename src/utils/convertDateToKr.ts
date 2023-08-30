26a0fd227f066e875c43c25bb4a926d8e8819687
devseop <cocc263@gmail.com>
2023-08-30 17:37:34 +0900

    STYLE: 날짜 유틸 리턴값 형식 변경

--- src/utils/convertDateToKr.ts
+++ src/utils/convertDateToKr.ts
@@ -1,8 +1,8 @@
-export const converDateToKr = (date: string) => {
+export const converDate = (date: string) => {
   const createdDate = new Date(date);
   const year = createdDate.getFullYear();
   const month = createdDate.getMonth();
   const day = createdDate.getDate();
 
-  return `${year}년 ${month}월 ${day}일`;
+  return `${year}. ${month}. ${day}`;
 };
